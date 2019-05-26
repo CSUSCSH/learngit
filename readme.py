f9ac12ca7cf2c1312b4292cf45e613df3702ce5c
lsb <CSUSCSH@163.COM>
2019-05-26 18:25:41 +0800

    branch test

--- readme.py
+++ readme.py
@@ -1,3 +1,6 @@
 Git is a distributed version control system.
 Git is free software distributed under the GPL.
 Git has a mutable index called stage.
+hahahahah
+kendiewanyi
+creating a new branch is quick.
